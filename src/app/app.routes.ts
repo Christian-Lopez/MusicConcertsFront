8dfa54a7812a45e3e67f6b9908f096f155adc103
Christian Lopez <cjavier93@gmail.com>
2025-09-28 23:35:50 -0600

    feat: :wrench: Configure firts routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { ForgotPassword } from './forgot-password/forgot-password';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: Home,
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: Login,
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    component: Register,
+  },
+  {
+    path: 'forgot-password',
+    pathMatch: 'full',
+    component: ForgotPassword,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
