6663027135597759d0d6f3544f0bde32f76ba458
Christian Lopez <cjavier93@gmail.com>
2025-09-28 23:14:40 -0600

    feat: :sparkles: Add Card Component and style changes

---
+++ src/app/home/concert-card/concert-card.ts
@@ -0,0 +1,10 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concert-card',
+  imports: [],
+  templateUrl: './concert-card.html',
+  styleUrl: './concert-card.css',
+})
+export class ConcertCard {}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/components/header/header';
 import { Footer } from '../shared/components/footer/footer';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { ConcertCard } from './concert-card/concert-card';
 
 @Component({
   selector: 'app-home',
-  imports: [Header,Footer],
+  imports: [Header, Footer, MatSelectModule, ConcertCard],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
