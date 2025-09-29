2e7f103195d55cf7c80af92a2fd26add93de31d1
Christian Lopez <cjavier93@gmail.com>
2025-09-29 11:38:17 -0600

    feat: :sparkles: Adding httpclient and testing fetch

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header } from '../shared/components/header/header';
 import { Footer } from '../shared/components/footer/footer';
 import { MatSelectModule } from '@angular/material/select';
@@ -10,4 +10,18 @@ import { ConcertCard } from './concert-card/concert-card';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  ngOnInit() :void {
+    console.log('Home component initialized');
+    // Example of fetching data from an API
+    // fetch('https://jsonplaceholder.typicode.com/todos')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log('Concerts data:', data);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching concerts data:', error);
+    //   });
+    //Using httpClient
+  }
+}
